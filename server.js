345e530374127bc0d0f3609f9b4158be2c3f4784
Paola Novick <paola.novick@davinci.edu.ar>
2024-09-14 18:51:46 -0300

    Añadida funcionalidad para manejar películas favoritas en la Parte 2

--- server.js
+++ server.js
@@ -6,27 +6,50 @@ const app = express();
 
 // Definir el puerto donde va a escuchar el servidor
 const PORT = 3000;
+
+// Array de películas favoritas
+const peliculasFavoritas = [
+  "Inception",
+  "The Matrix",
+  "Interstellar",
+  "The Shawshank Redemption",
+  "The Godfather",
+];
+
 // Ruta principal "/" que muestra nombre y apellido
-app.get('/', (req, res) => {
-    res.send('Nombre: Paola, Apellido: Novick');
+app.get("/", (req, res) => {
+  res.send("Nombre: Paola, Apellido: Novick");
 });
 
 // Ruta "/materia" que muestra información de la materia
-app.get('/materia', (req, res) => {
-    res.send('Materia: Aplicaciones Hibridas');
+app.get("/materia", (req, res) => {
+  res.send("Materia: Aplicaciones Hibridas");
 });
 
 // Ruta "/profesor" que muestra información del profesor
-app.get('/profesor', (req, res) => {
-    res.send("Profesor: Camila Belén Marcos Galban ");
+app.get("/profesor", (req, res) => {
+  res.send("Profesor: Camila Belén Marcos Galban ");
+});
+
+// Ruta para manejar películas
+app.get("/peliculas", (req, res) => {
+  // Obtener el parámetro de la consulta
+  const pelicula = req.query.nombre;
+
+  // Verificar si la película está en el array de favoritos
+  if (peliculasFavoritas.includes(pelicula)) {
+    res.send("La película seleccionada ya está en favoritos.");
+  } else {
+    res.status(404).send("404 – película no encontrada.");
+  }
 });
 
 // Ruta para cualquier otro recurso no encontrado
-app.get('*', (req, res) => {
-    res.status(404).send('Página no encontrada');
-});
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+app.get("*", (req, res) => {
+  res.status(404).send("Página no encontrada");
 });
 
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
